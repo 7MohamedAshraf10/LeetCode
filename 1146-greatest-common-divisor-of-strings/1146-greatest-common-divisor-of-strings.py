fe18945d24cb0fc638f27326c3f1b33cb6dc1637
Mohamed Ashraf Hassan <78963147+7MohamedAshraf10@users.noreply.github.com>
2023-10-12 14:42:07 +0300

    Time: 38 ms (68.17%), Space: 16.2 MB (87.41%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # Check if concatenated strings are equal or not, if not return ""
+        if str1 + str2 != str2 + str1:
+            return ""
+        # If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
+        from math import gcd
+        return str1[:gcd(len(str1), len(str2))]
