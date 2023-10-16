df043e1c114c55b9d90b0d8a915937b57ea95c26
Mohamed Ashraf Hassan <78963147+7MohamedAshraf10@users.noreply.github.com>
2023-10-17 01:41:51 +0300

    Time: 115 ms (99.98%), Space: 17.9 MB (27.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,6 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # the idea is to know the number of non-zeros elements
+        # then I would know the number of zero elements (length of list - number of non-zero)
+        # After that add (length of list - number of non-zero) zero elements to the list 
         count = 0
         for i in range(len(nums)):
             if nums[i] != 0 :
