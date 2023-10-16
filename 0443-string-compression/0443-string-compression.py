2f30c1181dd9e471c4e266d4ef392e3a9975de68
Mohamed Ashraf Hassan <78963147+7MohamedAshraf10@users.noreply.github.com>
2023-10-17 01:10:13 +0300

    Time: 70 ms (24.53%), Space: 16.5 MB (31.93%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,10 +1,14 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
+        # there's nothing to compress, so we return 0.
         if not chars:
             return 0
+        # track the current position where we are writing compressed characters
         write =0
+        # represents the count of consecutive characters.
         count = 1
         for i in range(1 , len(chars)):
+            #  check if the current character is the same as the previous character
             if chars[i] == chars[i - 1]:
                 count+=1
             else:
@@ -15,9 +19,10 @@ class Solution:
                         chars[write]=dig
                         write+=1
                 count = 1
-            
+        # handle the last character and its count    
         chars[write] = chars[-1]
         write += 1
+        #  if there's a count greater than 1 for the last character, we convert it to a string and split it into digits
         if count > 1:
             for digit in str(count):
                 chars[write] = digit
