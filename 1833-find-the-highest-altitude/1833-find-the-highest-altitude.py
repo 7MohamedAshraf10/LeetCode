7e085d911074c3afddcf64061c2771383efdfc8e
Mohamed Ashraf Hassan <78963147+7MohamedAshraf10@users.noreply.github.com>
2023-10-24 00:55:57 +0300

    Time: 52 ms (6.9%), Space: 16.3 MB (43.46%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
@@ -1,9 +1,14 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        
+        # length of the list 
         n = len(gain)
+        # list to get the altidute, and it's length = length of the gain + 1
         ans = [0]*(n+1)
+
+        # iterate through each variable in gain list
         for i in range(n):
+            # iterate through each value before the current value and sum them, and store them in the ans list
             for x in range(0 , i+1):
                 ans[i] += gain[x]
+        # return the max number in ans, which is the highest alitude
         return max(ans)
