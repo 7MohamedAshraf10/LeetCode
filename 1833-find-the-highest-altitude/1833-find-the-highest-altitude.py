76ca6f8e5d3415bf7df62050c3f6afd39643cce7
Mohamed Ashraf Hassan <78963147+7MohamedAshraf10@users.noreply.github.com>
2023-10-24 00:56:26 +0300

    Time: 37 ms (83.83%), Space: 16.1 MB (94.88%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
@@ -4,7 +4,6 @@ class Solution:
         n = len(gain)
         # list to get the altidute, and it's length = length of the gain + 1
         ans = [0]*(n+1)
-
         # iterate through each variable in gain list
         for i in range(n):
             # iterate through each value before the current value and sum them, and store them in the ans list
